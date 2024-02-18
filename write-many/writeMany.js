be5793cfbf301ea4ecd47124576801d7b30e7e88
S. M. Shakib Hossain <smsakib2403@gmail.com>
2024-02-18 16:24:52 +0600

    Write many

--- write-many/writeMany.js
+++ write-many/writeMany.js
@@ -1,3 +1,14 @@
-console.time('hello world');
-let  a = 2 + 2;
-console.timeEnd('hello world');
+const fs = require("node:fs/promises");
+
+// Execution time: 51.487s
+// CPU usage: 39% (7th gen i7)
+
+(async () => {
+    console.time("writeMany");
+    const filehandle = await fs.open("./write-many/test.txt", "w");
+
+    for (let i = 0; i < 1000000; i++) {
+        await filehandle.write(`test ${i}\n`);
+    }
+    console.timeEnd("writeMany");
+})();
