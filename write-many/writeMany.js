72625e692b7c63e76349de6fba2f7b5fb01c901c
S. M. Shakib Hossain <smsakib2403@gmail.com>
2024-02-19 17:15:26 +0600

    Streams version

--- write-many/writeMany.js
+++ write-many/writeMany.js
@@ -19,7 +19,7 @@
 //Sync Version
 // Execution time: 1.02ms
 // CPU usage: 83% (7th gen i7)
-
+/* 
 const fs = require("node:fs");
 
 (async () => {
@@ -30,5 +30,26 @@ const fs = require("node:fs");
             fs.writeSync(fd, buff);
         }
     });
+    console.timeEnd("writeMany");
+})(); */
+
+
+// Streams Version
+// Execution time: 888.741ms
+// CPU usage: 91% (7th gen i7)
+// Don't use it this way. It eats too much memeory.
+
+const fs = require("node:fs/promises");
+
+(async () => {
+    console.time("writeMany");
+    const filehandle = await fs.open("./write-many/test.txt", "w");
+
+    const stream = filehandle.createWriteStream();
+
+    for (let i = 0; i < 1000000; i++) {
+        const buff = Buffer.from(` ${i}\n`, "utf-8");
+        stream.write(buff);
+    }
     console.timeEnd("writeMany");
 })();
